0ec13126560922fec624d1469e728f944969733c
Clement Tech <kncshools@gmail.com>
2024-12-13 14:35:35 +0200

    Initial commit: project setup

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Clement <code>TECH</code> 250
+        </p>
+        <a
+          className="App-link"
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Contact Information
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
